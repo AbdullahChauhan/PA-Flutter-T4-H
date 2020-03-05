58ae05628136740578a9585f3d421a69f077e3b8
AbdullahChauhan <acse.uok@gmail.com>
2020-03-05 23:38:07 +0500

    appbar wavy header UI - elements(avatar, add, search) added

--- lib/widgets/custom_header.dart
+++ lib/widgets/custom_header.dart
@@ -3,15 +3,56 @@ import 'package:flutter/material.dart';
 class WavyAppHeader extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ClipPath(
-      child: Expanded(
-        child: Container(
-          color: Theme.of(context).accentColor,
-          width: double.infinity,
-          height: 100.0,
+    return Stack(
+      children: <Widget>[
+        ClipPath(
+          child: Container(
+            color: Theme.of(context).accentColor,
+            width: double.infinity,
+            height: 90.0,
+          ),
+          clipper: BottomWaveClipper(),
         ),
-      ),
-      clipper: BottomWaveClipper(),
+        Padding(
+          padding: EdgeInsets.only(left: 18.0, top: 14.0, bottom: 18.0),
+          child: Row(
+            children: <Widget>[
+              CircleAvatar(
+                backgroundColor: Colors.grey,
+                radius: 20.0,
+              ),
+              SizedBox(
+                width: 8.0,
+              ),
+              Text(
+                'Chats',
+                style: TextStyle(color: Colors.white, fontSize: 24.0),
+              ),
+              Spacer(),
+              GestureDetector(
+                child: Icon(Icons.add, size: 32.0, color: Colors.white),
+                onTap: () => print('Clicked!'),
+              ),
+              SizedBox(
+                width: 12.0,
+              ),
+              Container(
+                height: 35,
+                width: 50,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(40),
+                        bottomLeft: Radius.circular(40)),
+                    color: Theme.of(context).primaryColor),
+                child: GestureDetector(
+                  child: Icon(Icons.search, size: 28.0, color: Colors.white),
+                  onTap: () => print('Clicked!'),
+                ),
+              ),
+            ],
+          ),
+        )
+      ],
     );
   }
 }
