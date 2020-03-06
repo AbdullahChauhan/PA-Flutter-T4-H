77e6e434c311f1b90cab05c81b67ee1e012e2617
AbdullahChauhan <acse.uok@gmail.com>
2020-03-06 16:00:46 +0500

    avatar services implemented

--- lib/services/image_picker.dart
+++ lib/services/image_picker.dart
@@ -1,3 +1,10 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+import 'package:image_picker/image_picker.dart';
+
 class ImagePickerService {
-  
+  Future<File> pickImage({@required ImageSource source}) {
+    return ImagePicker.pickImage(source: source);
+  }
 }
